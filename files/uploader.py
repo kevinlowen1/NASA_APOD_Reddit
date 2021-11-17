485b0450edebc497bff6d04dd37529a9371c4d3a
kevinlowen <kevinlowen@gmail.com>
2021-11-16 20:39:06 -0500

    uploads text post to chosen subreddit

--- files/uploader.py
+++ files/uploader.py
@@ -1,41 +1,63 @@
+##Run refreshToken.py to get value to enter into config.py
+
 import config
 import requests
 import pandas
+import praw
 
-## Create variable for pandas dataframes
-df = pandas.DataFrame()
+# ## Create variable for pandas dataframes
+# df = pandas.DataFrame()
 
-## Create variables for reddit connection
-auth = requests.auth.HTTPBasicAuth(config.REDDIT_OAUTH_CLIENT_ID, config.REDDIT_SECRET_KEY)
-data = {
-    'grant_type': 'password',
-    'username': config.REDDIT_USERNAME,
-    'password': config.REDDIT_PASSWORD
-}
-headers = {
-    'User-Agent': 'MyAPI/0.0.1'
-}
+# ## Create variables for reddit connection
+# auth = requests.auth.HTTPBasicAuth(config.REDDIT_OAUTH_CLIENT_ID, config.REDDIT_SECRET_KEY)
+# data = {
+#     'grant_type': 'password',
+#     'username': config.REDDIT_USERNAME,
+#     'password': config.REDDIT_PASSWORD
+# }
+# headers = {
+#     'User-Agent': 'MyAPI/0.0.1'
+# }
 
-## Save token to headers variable
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-    auth = auth, data=data, headers=headers)
-TOKEN = res.json()['access_token']
-print(TOKEN)
-headers['Authorization'] = f'bearer {TOKEN}'
-print(headers['Authorization'])
+# ## Save token to headers variable
+# res = requests.post('https://www.reddit.com/api/v1/access_token',
+#     auth = auth, data=data, headers=headers)
+# TOKEN = res.json()['access_token']
+# #print(TOKEN)
+# headers['Authorization'] = f'bearer {TOKEN}'
+# #print(headers['Authorization'])
 
-## Get the top posts in Astrophotography subreddit
-res = requests.get('https://oauth.reddit.com/r/astrophotograhy/hot',headers=headers)
-# print(res.json())
-for post in res.json()['data']['children']:
-    df = df.append({
-        'subreddit': post['data']['subreddit'],
-        'title': post['data']['title'],
-        'selftext': post['data']['selftext'],
-        'upvote_ratio': post['data']['upvote_ratio'],
-        'ups': post['data']['ups'],
-        'downs': post['data']['downs'],
-        'score': post['data']['score']
-    }, ignore_index=True)
+# ## Get the top posts in Astrophotography subreddit
+# res = requests.get('https://oauth.reddit.com/r/politics/hot', headers=headers)
+# #print(res.json())
 
-print(df)
+# ## loop through response and save relevant parts to dataframe
+# for post in res.json()['data']['children']:
+#     df = df.append({
+#         'subreddit': post['data']['subreddit'],
+#         'title': post['data']['title'],
+#         'selftext': post['data']['selftext'],
+#         'upvote_ratio': post['data']['upvote_ratio'],
+#         'ups': post['data']['ups'],
+#         'downs': post['data']['downs'],
+#         'score': post['data']['score']
+#     }, ignore_index=True)
+
+#print(df)
+
+reddit = praw.Reddit(client_id=config.REDDIT_OAUTH_CLIENT_ID,
+                     client_secret=config.REDDIT_SECRET_KEY,
+                     user_agent='script by u/ilikecake123',
+                     redirect_uri=config.REDDIT_REDIRECT_URI,
+                     refresh_token=config.REDDIT_REFRESH_TOKEN)
+
+subr = 'ASTRONOMY_DAILY' # Choose your subreddit
+subreddit = reddit.subreddit(subr) # Initialize the subreddit to a variable
+title = 'THIS IS A TEST'
+selftext = '''
+I am learning how to use the Reddit API with Python using the PRAW wrapper.
+By following the tutorial on https://www.jcchouinard.com/post-on-reddit-api-with-python-praw/
+This post was uploaded from my Python Script
+'''
+
+subreddit.submit(title,selftext=selftext)
