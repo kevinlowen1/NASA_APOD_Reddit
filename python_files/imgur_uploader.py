9a915db89437abb8bbd04e8048e61826d6905138
kevinlowen <kevinlowen@gmail.com>
2021-11-19 20:44:14 -0500

    can pull images from front page of imgur

--- python_files/imgur_uploader.py
+++ python_files/imgur_uploader.py
@@ -1,29 +1,10 @@
-from base64 import b64encode
-import json
-import requests
+from imgurpython import ImgurClient
 import config
 
+client = ImgurClient(config.IMGUR_CLIENT_ID, config.IMGUR_CLIENT_SECRET)
+items = client.gallery()
 
-
-client_id = config.IMGUR_CLIENT_ID
-# headers = ['Authorization'] = f'Client-ID {client_id}'
-headers = {"Authorization": f'Client-ID {client_id}'}
-api_key = config.IMGUR_CLIENT_SECRET
-url = "https://api.imgur.com/3/upload.json"
-
-
-# print(r'..\media\test.jpg')
-
-j1 = requests.post(
-    url, 
-    headers = headers,
-    data = {
-        'key': api_key, 
-        #'image': b64encode(open(r'..\media\test.jpg', 'rb').read()),
-        'image': open(r'..\media\test.jpg', 'rb').read(),
-        'type': 'base64',
-        'name': 'test.jpg',
-        'title': 'Picture no. 1'
-    }
-)
-
+for item in items:
+    print(item.link)
+    print(item.title)
+    print(item.views)
