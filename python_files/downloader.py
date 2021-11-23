7fcec9febbb9d9d17272f3c634d4412a0c84cae9
kevinlowen <kevinlowen@gmail.com>
2021-11-22 19:48:42 -0500

    code working, reddit direct image upload

--- python_files/downloader.py
+++ python_files/downloader.py
@@ -2,21 +2,27 @@ import requests
 import config
 import urllib
 
-r               = requests.get('https://api.nasa.gov/planetary/apod?api_key=' + config.NASA_API_KEY)
-author          = r.json()['copyright']
-date            = r.json()['date']
-explanation     = r.json()['explanation']
-hdurl           = r.json()['hdurl']
-media_type      = r.json()['media_type']
-title           = r.json()['title']
-url             = r.json()['url']
+def nasa_apod_downloader():
+    r               = requests.get('https://api.nasa.gov/planetary/apod?api_key=' + config.NASA_API_KEY)
+    author          = r.json()['copyright']
+    date            = r.json()['date']
+    explanation     = r.json()['explanation']
+    hdurl           = r.json()['hdurl']
+    media_type      = r.json()['media_type']
+    title           = r.json()['title']
+    url             = r.json()['url']
 
-# print('author is      : ' + author        )
-# print('date is        : ' + date          )  
-# print('explanation is : ' + explanation   )
-# print('hdurl is       : ' + hdurl         )
-# print('media_type is  : ' + media_type    )
-# print('title is       : ' + title         )
-# print('url is         : ' + url           )
+    # print('author is      : ' + author        )
+    # print('date is        : ' + date          )  
+    # print('explanation is : ' + explanation   )
+    # print('hdurl is       : ' + hdurl         )
+    # print('media_type is  : ' + media_type    )
+    # print('title is       : ' + title         )
+    # print('url is         : ' + url           )
 
-testfile = urllib.request.urlretrieve(url, r'C:\temp\test.jpg')
+    testfile = urllib.request.urlretrieve(url, r'C:\temp\test.jpg')
+
+
+
+if __name__ == "__main__":
+   nasa_apod_downloader()
--- python_files/reddit_uploader.py
+++ python_files/reddit_uploader.py
@@ -1,64 +1,18 @@
-##Run refreshToken.py to get value to enter into config.py
-
 import config
-import requests
-import pandas
 import praw
 
-# ## Create variable for pandas dataframes
-# df = pandas.DataFrame()
+def reddit_uploader():
+    reddit = praw.Reddit(client_id=config.REDDIT_OAUTH_CLIENT_ID,
+                        client_secret=config.REDDIT_SECRET_KEY,
+                        user_agent='script by u/ilikecake123',
+                        redirect_uri=config.REDDIT_REDIRECT_URI,
+                        refresh_token=config.REDDIT_REFRESH_TOKEN)
 
-# ## Create variables for reddit connection
-# auth = requests.auth.HTTPBasicAuth(config.REDDIT_OAUTH_CLIENT_ID, config.REDDIT_SECRET_KEY)
-# data = {
-#     'grant_type': 'password',
-#     'username': config.REDDIT_USERNAME,
-#     'password': config.REDDIT_PASSWORD
-# }
-# headers = {
-#     'User-Agent': 'MyAPI/0.0.1'
-# }
+    subr = 'ASTRONOMY_DAILY' # Choose your subreddit
+    title = 'THIS IS SO NOT A TEST'
 
-# ## Save token to headers variable
-# res = requests.post('https://www.reddit.com/api/v1/access_token',
-#     auth = auth, data=data, headers=headers)
-# TOKEN = res.json()['access_token']
-# #print(TOKEN)
-# headers['Authorization'] = f'bearer {TOKEN}'
-# #print(headers['Authorization'])
+    image = r'C:\temp\test.jpg'
+    reddit.subreddit(subr).submit_image(title, image)
 
-# ## Get the top posts in Astrophotography subreddit
-# res = requests.get('https://oauth.reddit.com/r/politics/hot', headers=headers)
-# #print(res.json())
-
-# ## loop through response and save relevant parts to dataframe
-# for post in res.json()['data']['children']:
-#     df = df.append({
-#         'subreddit': post['data']['subreddit'],
-#         'title': post['data']['title'],
-#         'selftext': post['data']['selftext'],
-#         'upvote_ratio': post['data']['upvote_ratio'],
-#         'ups': post['data']['ups'],
-#         'downs': post['data']['downs'],
-#         'score': post['data']['score']
-#     }, ignore_index=True)
-
-#print(df)
-
-reddit = praw.Reddit(client_id=config.REDDIT_OAUTH_CLIENT_ID,
-                     client_secret=config.REDDIT_SECRET_KEY,
-                     user_agent='script by u/ilikecake123',
-                     redirect_uri=config.REDDIT_REDIRECT_URI,
-                     refresh_token=config.REDDIT_REFRESH_TOKEN)
-
-subr = 'ASTRONOMY_DAILY' # Choose your subreddit
-title = 'THIS IS A TEST'
-selftext = 'test of text post'
-
-# subreddit = reddit.subreddit(subr) # Initialize the subreddit to a variable
-
-
-# subreddit.submit(title,selftext=selftext)
-
-image = r'C:\Users\Kevin.DESKTOP-9D0VMK8\Documents\Projects\NASA_APOD_Reddit\python_files\test.jpg'
-reddit.subreddit(subr).submit_image(title, image)
+if __name__ == "__main__":
+   reddit_uploader()
